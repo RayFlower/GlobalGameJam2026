7a2a05847c7461ee983ebb91d2375b85978746c6
Bryan Tai <BryanTai@users.noreply.github.com>
2026-01-30 23:58:24 -0800

    Added page dragging logic and main scene

---
+++ global-game-jam-2026/drag_panel_container.gd
@@ -0,0 +1,27 @@
+extends PanelContainer
+
+var button_pressed:bool = false
+var previous_mouse_position:Vector2 = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if button_pressed:
+		var current_mouse_position = get_viewport().get_mouse_position()
+		var mouse_delta = current_mouse_position - previous_mouse_position
+		position += mouse_delta
+		previous_mouse_position = current_mouse_position
+	
+
+
+func _on_drag_button_button_down() -> void:
+	button_pressed = true
+	previous_mouse_position = get_viewport().get_mouse_position()
+
+
+func _on_drag_button_button_up() -> void:
+	button_pressed = false
