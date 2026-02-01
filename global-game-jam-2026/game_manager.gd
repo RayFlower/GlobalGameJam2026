231d91b1797f9494aafce954a98a11cf0c108198
Bryan Tai <BryanTai@users.noreply.github.com>
2026-01-31 19:24:50 -0800

    Load puzzle answers into decrypter

--- global-game-jam-2026/game_manager.gd
+++ global-game-jam-2026/game_manager.gd
@@ -11,23 +11,26 @@ var draggable_scene = load("res://draggable.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	load_pages(0)
+	load_puzzle(0)
 	
-#func load_puzzle(index: int):
-	
-
-func load_pages(index: int):
+func load_puzzle(index: int):
 	var current_puzzle = puzzle_data[index]
+	var all_answers_array = current_puzzle.all_comma_seperated_answer_texts
+	message_decryption.assign_cipher_data(all_answers_array)
 	
-	for page_texture in current_puzzle.page_textures:
+	load_pages(current_puzzle.page_textures)
+
+func load_pages(textures: Array[Texture2D]):
+
+	for page_texture in textures:
 		var new_draggable = draggable_scene.instantiate() as Draggable
 		new_draggable.initialize(page_texture)
 		#TODO set random position
 		draggable_area.add_new_draggable(new_draggable)
 
 func on_correct_answer() -> void:
-	current_puzzle_index = current_puzzle_index + 1
-	load_pages(current_puzzle_index)
+	current_puzzle_index += 1
+	load_puzzle(current_puzzle_index)
 	
 func _on_answer_correct_debug_button_down() -> void:
 	on_correct_answer()
--- global-game-jam-2026/message_decryption.gd
+++ global-game-jam-2026/message_decryption.gd
@@ -5,26 +5,27 @@ extends Node2D
 @onready var CipherText_1_text 			= $"Cipher Selection 1/Cipher Text"
 @onready var CipherText_2_text 			= $"Cipher Selection 2/Cipher Text"
 @onready var CipherText_3_text 			= $"Cipher Selection 3/Cipher Text"
+@onready var CipherText_4_text			= $"Cipher Selection 4/Cipher Text"
 
-## Parameters
+@onready var all_cipher_texts = [CipherText_1_text,CipherText_2_text,CipherText_3_text,CipherText_4_text]
+@onready var all_cipher_controls = [$"Cipher Selection 1", $"Cipher Selection 2",$"Cipher Selection 3",$"Cipher Selection 4"]
 
-## Instantiate
 var cipher_1_index = 0
 var cipher_2_index = 0
 var cipher_3_index = 0
+var cipher_4_index = 0
 
 var cipher_1_answer_list = []
 var cipher_2_answer_list = []
 var cipher_3_answer_list = []
+var cipher_4_answer_list = []
 
+var all_cipher_lists = [cipher_1_answer_list,cipher_2_answer_list,cipher_3_answer_list,cipher_4_answer_list]
+var current_correct_answers = ["","","",""]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	assign_cipher_data()
-	get_random_starting_selection()
-	
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -34,45 +35,25 @@ func _process(delta: float) -> void:
 
 ## --- Functions ------------------------------------------
 
-func assign_cipher_data() -> void:
+func assign_cipher_data(all_answers_texts: Array[String]) -> void:
 	# Assigns the current puzzle data from the game manager to the message decryptor object
 	
-	cipher_1_answer_list = [
-		"hewwo",
-		"0w0",
-		"what's this?",
-	]
+	for cipher_control in all_cipher_controls:
+		cipher_control.visible = false
 	
-	cipher_2_answer_list = [
-		"fuck you",
-		"fuck me",
-		"no, fuck you!",
-	]
+	var current_index = 0
+	for answers_text in all_answers_texts:
+		var answers_split:Array[String]
+		answers_split.assign(answers_text.split(","))
+		current_correct_answers[current_index] = answers_split[0]
+		
+		answers_split.shuffle()
+		all_cipher_lists[current_index].assign(answers_split)
+		all_cipher_controls[current_index].visible = true
+		all_cipher_texts[current_index].text = answers_split[0]
+		
+		current_index += 1
 	
-	cipher_3_answer_list = [
-		"1",
-		"2",
-		"3",
-		"4",
-		"5",
-	]
-	pass
-
-func get_random_starting_selection() -> void:
-	if(cipher_1_answer_list.size() == 0) : pass
-	
-	# Get random selection of answers and fill into selection fields
-	cipher_1_index = randi_range(0, cipher_1_answer_list.size() - 1)
-	cipher_2_index = randi_range(0, cipher_2_answer_list.size() - 1)
-	cipher_3_index = randi_range(0, cipher_3_answer_list.size() - 1)
-	
-	# Get text from index
-	CipherText_1_text.text = cipher_1_answer_list[cipher_1_index]
-	CipherText_2_text.text = cipher_2_answer_list[cipher_2_index]
-	CipherText_3_text.text = cipher_3_answer_list[cipher_3_index]
-	pass
-
-
 
 ## --- BUTTON SIGNALS --------------------------------------
 
@@ -104,6 +85,16 @@ func _on_cypher3_button_previous_pressed() -> void:
 func _on_cypher3_button_next_pressed() -> void:
 	cipher_3_index = (cipher_3_index + 1) % cipher_3_answer_list.size()
 	CipherText_3_text.text = cipher_3_answer_list[cipher_3_index]
+	pass # Replace with function body.
+	
+func _on_cypher4_button_previous_pressed() -> void:
+	cipher_4_index = ((cipher_4_index - 1) + cipher_4_answer_list.size()) % cipher_4_answer_list.size()
+	CipherText_4_text.text = cipher_4_answer_list[cipher_4_index]
+	pass # Replace with function body.
+	
+func _on_cypher4_button_next_pressed() -> void:
+	cipher_4_index = (cipher_4_index + 1) % cipher_4_answer_list.size()
+	CipherText_4_text.text = cipher_4_answer_list[cipher_4_index]
 	pass # Replace with function body.
 
 func _on_send_button_pressed() -> void:
