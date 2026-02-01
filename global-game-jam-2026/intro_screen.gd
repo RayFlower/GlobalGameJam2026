4b2d5a648620c4e7bb632332c81aa9aee874e031
Bryan Tai <redhatpieman@gmail.com>
2026-02-01 13:20:50 -0800

    Updated intro screen with new assets and cypher

--- global-game-jam-2026/intro_screen.gd
+++ global-game-jam-2026/intro_screen.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
+@export var draggable_area: CanvasLayer
+var draggable_scene = load("res://draggable.tscn")
+@export var main_menu_cypher_texture:Texture2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize();
-	pass # Replace with function body.
+	var new_draggable = draggable_scene.instantiate() as Draggable
+	new_draggable.initialize(main_menu_cypher_texture)
+	new_draggable.position = Vector2(1200,700)
+	draggable_area.add_new_draggable(new_draggable)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
