b0793637058581c3abde2aebe8b3fe152a4d8f68
RayFlower\LenovoLaptop <colebeernaerts@gmail.com>
2026-01-31 16:54:41 -0800

    Results screen

---
+++ global-game-jam-2026/results_screen.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+@onready var result_stat = $"result stat"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	set_result_stat()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+## --- Functions --------------------------------------------
+
+func set_result_stat() -> void:
+	
+	# Get stats
+	var _solved_cyphers = 0
+	var _total_cyphers = 99
+	
+	# set text to display
+	result_stat.text = str(_solved_cyphers) + " / " + str(_total_cyphers)
+	pass
+
+
+
+## --- Signals ----------------------------------------------
+
+func _on_retry_button_pressed() -> void:
+	# Restart the game
+	
+	
+	pass # Replace with function body.
